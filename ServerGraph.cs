04095e797047a34f9c156adb157828d3ac81746e
Farzad I. Student <theeonsamurai@gmail.com>
2024-02-01 21:38:42 -0500

    added critical servers

--- ServerGraph.cs
+++ ServerGraph.cs
@@ -113,8 +113,98 @@ public class ServerGraph : WebGraph
 
     public string[] CriticalServers()
     {
-        return new string[] { "test" };
+        // Mark all the vertices as not visited
+		bool[] visited = new bool[V];
+		int[] disc = new int[V];
+		int[] low = new int[V];
+		int[] parent = new int[V];
+		bool[] ap = new bool[V]; // To store articulation points
+		string[] result = new string[V];
+		int j = 0;
+
+		// Initialize parent and visited, and
+		// ap(articulation point) arrays
+		for (int i = 0; i < V; i++)
+		{
+			parent[i] = NIL;
+			visited[i] = false;
+			ap[i] = false;
+		}
+
+		// Call the recursive helper function to find articulation
+		// points in DFS tree rooted with vertex 'i'
+		for (int i = 0; i < V; i++)
+			if (visited[i] == false)
+				CSRecurse(i, visited, disc, low, parent, ap);
+
+		// Now ap[] contains articulation points, print them
+		for (int i = 0; i < V; i++)
+		{
+			if (ap[i] == true) 
+			{
+				Console.Write(i + " ");
+				result[j] = i.ToString();
+				j++;
+
+			}
+
+		}
+		return result;
     }
+    
+    // A recursive function that find articulation points using DFS
+	// u --> The vertex to be visited next
+	// visited[] --> keeps track of visited vertices
+	// disc[] --> Stores discovery times of visited vertices
+	// parent[] --> Stores parent vertices in DFS tree
+	// ap[] --> Store articulation points
+	void CSRecurse(int u, bool[] visited, int[] disc,
+				int[] low, int[] parent, bool[] ap)
+	{
+
+		// Count of children in DFS Tree
+		int children = 0;
+
+		// Mark the current node as visited
+		visited[u] = true;
+
+		// Initialize discovery time and low value
+		disc[u] = low[u] = ++time;
+
+		// Go through all vertices adjacent to this
+		foreach (int i in adj[u])
+		{
+			int v = i; // v is current adjacent of u
+
+			// If v is not visited yet, then make it a child of u
+			// in DFS tree and recur for it
+			if (!visited[v])
+			{
+				children++;
+				parent[v] = u;
+				CSRecurse(v, visited, disc, low, parent, ap);
+
+				// Check if the subtree rooted with v has
+				// a connection to one of the ancestors of u
+				low[u] = Math.Min(low[u], low[v]);
+
+				// u is an articulation point in following cases
+
+				// (1) u is root of DFS tree and has two or more children.
+				if (parent[u] == NIL && children > 1)
+					ap[u] = true;
+
+				// (2) If u is not root and low value of one of its child
+				// is more than discovery value of u.
+				if (parent[u] != NIL && low[v] >= disc[u])
+					ap[u] = true;
+			}
+
+			// Update low value of u for parent function calls.
+			else if (v != parent[u])
+				low[u] = Math.Min(low[u], disc[v]);
+		}
+	}
 
     public int ShortestPath(string from, string to)
     {
