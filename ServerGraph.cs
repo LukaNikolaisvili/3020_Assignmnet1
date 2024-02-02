4008bbb478dc63e3986f21e3d9d7740dd279513d
LukaNikolaisvili <jokerpoker1213@gmail.com>
2024-02-01 21:41:36 -0500

    testing main method

--- ServerGraph.cs
+++ ServerGraph.cs
@@ -113,98 +113,8 @@ public class ServerGraph : WebGraph
 
     public string[] CriticalServers()
     {
-        // Mark all the vertices as not visited
-		bool[] visited = new bool[V];
-		int[] disc = new int[V];
-		int[] low = new int[V];
-		int[] parent = new int[V];
-		bool[] ap = new bool[V]; // To store articulation points
-		string[] result = new string[V];
-		int j = 0;
-
-		// Initialize parent and visited, and
-		// ap(articulation point) arrays
-		for (int i = 0; i < V; i++)
-		{
-			parent[i] = NIL;
-			visited[i] = false;
-			ap[i] = false;
-		}
-
-		// Call the recursive helper function to find articulation
-		// points in DFS tree rooted with vertex 'i'
-		for (int i = 0; i < V; i++)
-			if (visited[i] == false)
-				CSRecurse(i, visited, disc, low, parent, ap);
-
-		// Now ap[] contains articulation points, print them
-		for (int i = 0; i < V; i++)
-		{
-			if (ap[i] == true) 
-			{
-				Console.Write(i + " ");
-				result[j] = i.ToString();
-				j++;
-
-			}
-
-		}
-		return result;
+        return new string[] { "test" };
     }
-    
-    // A recursive function that find articulation points using DFS
-	// u --> The vertex to be visited next
-	// visited[] --> keeps track of visited vertices
-	// disc[] --> Stores discovery times of visited vertices
-	// parent[] --> Stores parent vertices in DFS tree
-	// ap[] --> Store articulation points
-	void CSRecurse(int u, bool[] visited, int[] disc,
-				int[] low, int[] parent, bool[] ap)
-	{
-
-		// Count of children in DFS Tree
-		int children = 0;
-
-		// Mark the current node as visited
-		visited[u] = true;
-
-		// Initialize discovery time and low value
-		disc[u] = low[u] = ++time;
-
-		// Go through all vertices adjacent to this
-		foreach (int i in adj[u])
-		{
-			int v = i; // v is current adjacent of u
-
-			// If v is not visited yet, then make it a child of u
-			// in DFS tree and recur for it
-			if (!visited[v])
-			{
-				children++;
-				parent[v] = u;
-				CSRecurse(v, visited, disc, low, parent, ap);
-
-				// Check if the subtree rooted with v has
-				// a connection to one of the ancestors of u
-				low[u] = Math.Min(low[u], low[v]);
-
-				// u is an articulation point in following cases
-
-				// (1) u is root of DFS tree and has two or more children.
-				if (parent[u] == NIL && children > 1)
-					ap[u] = true;
-
-				// (2) If u is not root and low value of one of its child
-				// is more than discovery value of u.
-				if (parent[u] != NIL && low[v] >= disc[u])
-					ap[u] = true;
-			}
-
-			// Update low value of u for parent function calls.
-			else if (v != parent[u])
-				low[u] = Math.Min(low[u], disc[v]);
-		}
-	}
 
     public int ShortestPath(string from, string to)
     {
@@ -236,16 +146,30 @@ class Program
         WebGraph webGraph = new WebGraph();
         WebPage page1 = new WebPage("name1", "localhost");
         // Step 2: Add three servers
-        serverGraph.AddServer("Server1", "Server1");
+        serverGraph.AddServer("Server1", "Server2");
+        serverGraph.AddServer("Server2", "Server1");
+        serverGraph.AddServer("Server3", "Server2");
 
-        Console.WriteLine(serverGraph.AddConnection("server1", "server2"));
-        serverGraph.PrintGraph();
-        Console.WriteLine(serverGraph.RemoveServer("server1", "server2"));
-        Console.WriteLine(serverGraph.AddWebPage(page1,"name"));
+        // 2. Add webpages
+        WebPage page2 = new WebPage("Page2", "Server2");
+        WebPage page3 = new WebPage("Page3", "Server3");
 
+        webGraph.AddPage("Page1", "Server1", serverGraph);
+        webGraph.AddPage("Page2", "Server2", serverGraph);
+        webGraph.AddPage("Page3", "Server3", serverGraph);
+
+        // 3. Add connections between servers
+        serverGraph.AddConnection("Server1", "Server2");
+        serverGraph.AddConnection("Server2", "Server3");
 
         //Take a look at this method...
         serverGraph.PrintGraph();
+
+         string[] result1 = serverGraph.CriticalServers();
+        for (int i = 0; i < result1.Length; i++)
+    {
+        Console.Write(result1[i] + " ");
+    }
     }
 
 }
