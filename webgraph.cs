25619646bcd0f12b35355209f1fb636f95609966
Frederick Nkwonta <frederickn076@gmail.com>
2024-02-05 02:32:10 -0500

    comment changes

--- webgraph.cs
+++ webgraph.cs
@@ -22,6 +22,7 @@ public class WebPage
     public string Server { get; set; }
     public List<WebPage> E { get; set; }
 
+
     // Constructors
     public WebPage(string name, string host)
     {
@@ -31,6 +32,8 @@ public class WebPage
 
     }
 
+    // 2 marks
+    // Return the index of the webpage with the given name; otherwise return -1
     // Method to find the current index location of a link
     public int FindLink(string name)
     {
@@ -77,10 +80,14 @@ public class WebGraph
         return -1; // Return -1 if not found
     }
 
-    // Adds a webpage with the given name, attached to the host server, 
-    // and passed with a servergaph object to work with
+    // 4 marks
+    // Add a webpage with the given name and store it on the host server
+    // Return true if successful; otherwise return false
+
     public bool AddPage(string name, string host, ServerGraph S)
+
     {
+        //check if name parmeter pass are empty or null 
         if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(host) || S == null)
         {
             return false;
@@ -100,7 +107,7 @@ public class WebGraph
                 Console.WriteLine(name + " is hosted on " + host);
                 return true;
             }
-            else
+            else // if server does not exists.
             {
                 Console.WriteLine("Server does not exist.");
                 return false;
@@ -108,27 +115,30 @@ public class WebGraph
         }
     }
 
-    //this method will remove the page based on the page name that we will provide to it
+    // 8 marks
+    // Remove the webpage with the given name, including the hyperlinks
+    // from and to the webpage
+    // Return true if successful; otherwise return false
     public bool RemovePage(string name, ServerGraph S)
     {
         int pageIndex = FindPage(name);
-        if (pageIndex == -1) 
-        return false; // Webpage not found
+        if (pageIndex == -1)
+            return false; // Webpage not found
 
-       //Webpage object pageToremove will be assigned to P[pageIndex]
+        //Webpage object pageToremove will be assigned to P[pageIndex]
         WebPage pageToRemove = P[pageIndex];
         //and with the method remove at we will remove with index
         P.RemoveAt(pageIndex);
 
-       
+
         foreach (var page in P)
-        {   
+        {
             //starting from the end and going to the beginning(reversed) basically 
             for (int i = page.E.Count - 1; i >= 0; i--)
-            {   
+            {
                 //if page at index I name is the same as the name that we want to remove 
                 if (page.E[i].Name == name)
-                {   
+                {
                     //then we will go ahead and remove that index
                     page.E.RemoveAt(i);
                 }
@@ -142,55 +152,59 @@ public class WebGraph
         return removedFromServer;
     }
 
+    // 3 marks
     // Adds a link that connects one webpage to another
     // It's like a server connection but for webpages! :D
     public bool AddLink(string from, string to)
-{
-    int indexFrom = FindPage(from);
-    int indexTo = FindPage(to);
-
-    // Check if both pages exist
-    if (indexFrom == -1 || indexTo == -1)
     {
-        return false; // since its or, and we want to link pages, it will return false, because true and false is true...
+        int indexFrom = FindPage(from);
+        int indexTo = FindPage(to);
+
+        // Check if both pages exist
+        if (indexFrom == -1 || indexTo == -1)
+        {
+            return false; // since its or, and we want to link pages, it will return false, because true and false is true...
+        }
+
+        // Check for an existing link to avoid duplication
+        var existingLink = P[indexFrom].E.Find(page => page.Name == to);
+        if (existingLink != null)
+        {
+            return false; // returns false because Link already there.
+        }
+
+        P[indexFrom].E.Add(P[indexTo]);
+
+        return true;
     }
 
-    // Check for an existing link to avoid duplication
-    var existingLink = P[indexFrom].E.Find(page => page.Name == to);
-    if (existingLink != null)
-    {
-        return false; // returns false because Link already there.
-    }
-
-    P[indexFrom].E.Add(P[indexTo]);
-
-    return true;
-}
-
+    // 3 marks
+    // Remove a hyperlink from one webpage to another
+    // Return true if successful; otherwise return false
     // Removes a link from the starting page that connects to the ending page if found
     public bool RemoveLink(string from, string to)
-{
-       //indexes of from to TO
-    int indexFrom = FindPage(from);
-    int indexTo = FindPage(to);
-
-       //checking if both are not equal to one 
-    if (indexFrom != -1 && indexTo != -1)
     {
-      //if indexFrom contains IndexTo
-        if (P[indexFrom].E.Contains(P[indexTo]))
-        {
-               //we will remove link between them
-            P[indexFrom].E.Remove(P[indexTo]);
-            
-                //return true
-            return true;
-        }
-    }
+        //indexes of from to TO
+        int indexFrom = FindPage(from);
+        int indexTo = FindPage(to);
 
-    //return false if there is no link or page does not exist at all
-    return false;
-}
+        //checking if both are not equal to one 
+        if (indexFrom != -1 && indexTo != -1)
+        {
+            //if indexFrom contains IndexTo
+            if (P[indexFrom].E.Contains(P[indexTo]))
+            {
+                //we will remove link between them
+                P[indexFrom].E.Remove(P[indexTo]);
+
+                //return true
+                return true;
+            }
+        }
+
+        //return false if there is no link or page does not exist at all
+        return false;
+    }
 
 
     // 6 marks
@@ -230,6 +244,8 @@ public class WebGraph
             return -1;
         }
 
+        //finds the shortest average.
+
         float avg = (float)shortestPath / P[locatePage].E.Count;
 
         return avg;
@@ -239,16 +255,17 @@ public class WebGraph
 
 
 
-    // Prints the webgraph
+    // 3 marks
+    // Print the name and hyperlinks of each webpage
     public void PrintGraph()
     {
-        foreach (WebPage page in P)
+        foreach (WebPage page in P) //look at all the pages 
         {
             Console.WriteLine("Webpage: " + page.Name);
             if (page.E.Count > 0)
             {
                 Console.WriteLine("Hyperlinks:");
-                foreach (WebPage hyperlink in page.E)
+                foreach (WebPage hyperlink in page.E) //look at all the connections 
                 {
                     Console.WriteLine("- " + hyperlink.Name);
                 }
